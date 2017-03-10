96dea9c838bcc6e6b3f1bdc8ad033107d980672b
William Riancho <william@reptilians.io>
2017-03-10 20:11:09 +0700

    wip

--- api/src/routes/oauth/revoke.js
+++ api/src/routes/oauth/revoke.js
@@ -10,26 +10,22 @@ export const post = [
     const auth = basicAuth(req);
 
     if (!auth) {
-      console.log( errors.Unauthorized );
       throw errors.Unauthorized;
     }
 
     if (auth.name !== '0e7814b6-b793-44f5-aab0-9644be51f1ae' || auth.pass !== '') {
-      console.log('toto', errors.Unauthorized);
       throw errors.Unauthorized;
     }
 
     const { token } = req.body;
 
     if (typeof token !== 'string' || token.length < 1) {
-      console.log('titi', errors.BadRequest);
       throw errors.BadRequest;
     }
 
     const accessToken = await AccessToken.find(token);
 
     if (!accessToken) {
-      console.log('totototto', errors.ResourceNotFound);
       throw errors.NotFound;
     }
 
