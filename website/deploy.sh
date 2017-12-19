4304f1c38763d12b9fabc79dc01cf140b5a4c4be
William Riancho <william@reptilians.io>
2017-12-20 00:42:52 +0100

    wip

--- website/deploy.sh
+++ website/deploy.sh
@@ -4,4 +4,4 @@ rm -rf ./dist
 ember build --prod
 find . -name .DS_Store -exec rm -f {} \;
 scp -r ./dist/assets william@prestine.io:/home/william/app/prestine/static
-scp ./dist/index.html william@prestine.io:/home/william/app/prestine/app/public
+scp ./dist/*.html william@prestine.io:/home/william/app/prestine/app/public
