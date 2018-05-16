94a563c6aa8fbc505d6616fe4f30293365fe7431
William Riancho <william@reptilians.io>
2018-05-16 18:57:44 +0200

    missions wip

---
+++ migrations/028_create-missions-table.js
@@ -0,0 +1,40 @@
+function up(knex) {
+  return knex.schema.createTable('missions', (table) => {
+    table.string('id').primary();
+
+    table.integer('price');
+    table.enum('price_currency', ['eur']);
+
+    table
+      .uuid('provider_id')
+      .references('users.id')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+
+    table
+      .uuid('client_id')
+      .references('users.id')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+
+    table
+      .uuid('service_category_id')
+      .references('service_categories.id')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+
+    table.dateTime('start_date');
+    table.dateTime('end_date');
+
+    table.boolean('accepted');
+
+    table.dateTime('created_at');
+    table.dateTime('updated_at');
+  });
+}
+
+function down(knex) {
+  return knex.schema.dropTable('provider_prices');
+}
+
+module.exports = { up, down };
