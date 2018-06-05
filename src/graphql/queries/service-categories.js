56a3fa86d085079bfd4e32a0104a10cc4dd167f6
William Riancho <william@reptilians.io>
2018-06-05 18:27:35 +0200

    wip

--- src/graphql/queries/service-categories.js
+++ src/graphql/queries/service-categories.js
@@ -1,24 +1,53 @@
 import ServiceCategory   from '../../models/service-category';
+import ProviderPrice     from '../../models/provider-price';
 import { registerQuery } from '../registry';
 
 const def = 'serviceCategories: [ServiceCategory]';
 
-const serviceCategories = async () => {
+const serviceCategories = async (_, __, { user }) => {
   const categories = await ServiceCategory
     .query((qb) => {
       qb.orderBy('ordinal_position');
     })
     .fetchAll();
+
+  const categoryIds = categories.map(category => category.get('id'));
+  const currentUserPrices = {};
+
+  if (user && user.get('isProvider')) {
+    const prices = await ProviderPrice
+      .query((qb) => {
+        qb.where('user_id', user.get('id'));
+        qb.whereIn('service_category_id', categoryIds);
+      })
+      .fetchAll();
+
+    prices.forEach((price) => {
+      currentUserPrices[price.get('serviceCategoryId')] = {
+        price: price.get('price'),
+        currency: price.get('currency')
+      };
+    });
+  }
+
   const rootCategories = categories.filter(category => category.get('parentId') === null);
 
   const categoryMapper = (category) => {
     const attr = category.serialize();
-
     attr.subCategories = categories
       .filter(subCat => subCat.get('parentId') === category.get('id'))
       .map(categoryMapper);
+
+    if (currentUserPrices[attr.id]) {
+      attr.providerPrice = currentUserPrices[attr.id].price;
+      attr.providerPriceCurrency = currentUserPrices[attr.id].currency;
+    } else {
+      attr.providerPrice = null;
+      attr.providerPriceCurrency = null;
+    }
     return attr;
   };
+
   return rootCategories.map(categoryMapper);
 };
 
--- src/graphql/types/service-category.js
+++ src/graphql/types/service-category.js
@@ -1,4 +1,5 @@
 import { registerType } from '../registry';
+import ProviderPrice    from '../../models/provider-price';
 import ServiceCategory  from '../../models/service-category';
 
 const def = `
@@ -7,12 +8,14 @@ type ServiceCategory {
   slug: String!
   color: String
   subCategories: [ServiceCategory]
+  providerPrice: Int
+  providerPriceCurrency: Currency
 }
 `;
 
 const resolver = {
   ServiceCategory: {
-    subCategories: async ({ id }) => {
+    subCategories: async ({ id }, params, { user }) => {
       const categories = await ServiceCategory
         .query((qb) => {
           qb.where('parent_id', '=', id);
@@ -20,7 +23,42 @@ const resolver = {
         })
         .fetchAll();
 
-      return categories.toArray().map(category => category.serialize());
+      const categoryIds = categories.map(category => category.get('id'));
+      const currentUserPrices = {};
+
+      if (user && user.get('isProvider')) {
+        const prices = await ProviderPrice
+          .query((qb) => {
+            qb.where('user_id', user.get('id'));
+            qb.whereIn('service_category_id', categoryIds);
+          })
+          .fetchAll();
+
+        prices.forEach((price) => {
+          currentUserPrices[price.get('serviceCategoryId')] = {
+            price: price.get('price'),
+            currency: price.get('currency')
+          };
+        });
+      }
+
+      const categoryMapper = (category) => {
+        const attr = category.serialize();
+        attr.subCategories = categories
+          .filter(subCat => subCat.get('parentId') === category.get('id'))
+          .map(categoryMapper);
+
+        if (currentUserPrices[attr.id]) {
+          attr.providerPrice = currentUserPrices[attr.id].price;
+          attr.providerPriceCurrency = currentUserPrices[attr.id].currency;
+        } else {
+          attr.providerPrice = null;
+          attr.providerPriceCurrency = null;
+        }
+        return attr;
+      };
+
+      return categories.map(categoryMapper);
     }
   }
 };
