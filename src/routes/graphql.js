f178be442a9fd5338eb3fdde4a14fa05f62fbd64
William Riancho <william@reptilians.io>
2018-06-18 14:33:54 +0200

    debug user removed

--- src/routes/graphql.js
+++ src/routes/graphql.js
@@ -2,7 +2,6 @@ import bodyParser         from 'body-parser';
 import { graphqlExpress } from 'apollo-server-express';
 import { formatError, GraphQLError } from 'graphql';
 import oauth2             from '../middlewares/oauth2';
-import User               from '../models/user';
 import getSchema          from '../graphql/schema';
 import { InternalError }  from '../graphql/errors';
 
@@ -12,14 +11,11 @@ export default () => {
   return [
     bodyParser.json(),
     async (req, res, next) => {
-      req.user = await User.find('3c656ce5-1e21-4332-a268-d7599f2f0e40');
-      next();
-
-      // let authorization = req.get('authorization');
-      // if (authorization) {
-      //   return oauth2(req, res, next);
-      // }
-      // return next();
+      let authorization = req.get('authorization');
+      if (authorization) {
+        return oauth2(req, res, next);
+      }
+      return next();
     },
     async (req, res, next) => {
       graphqlExpress({
