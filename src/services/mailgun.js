61ffed32d7492d4561ce06742a3c248b6f2078ef
William Riancho <william@reptilians.io>
2018-06-25 11:42:40 +0200

    fix email with no attachment

--- src/services/mailgun.js
+++ src/services/mailgun.js
@@ -2,20 +2,24 @@ import request from 'request-promise';
 import config from '../config';
 
 export const sendEmail = ({ from, to, subject, message, attachment }) => {
+  const formData = {
+    from,
+    to,
+    subject,
+    html: message,
+  };
+
+  if (attachment) {
+    formData.attachment = attachment;
+  }
+
   return request({
     method: 'POST',
     uri: `${config.mailgun.apiBaseURL}/messages`,
-
+    formData,
     auth: {
       user: 'api',
       pass: config.mailgun.apiKey
-    },
-    formData: {
-      from,
-      to,
-      subject,
-      html: message,
-      attachment
     }
   });
 };
