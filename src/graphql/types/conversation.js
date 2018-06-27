00e365f06fc5cd220b4ecd604a5bae7a68fa8137
William Riancho <william@reptilians.io>
2018-06-27 15:20:50 +0200

    fix missions serialization issue

--- src/graphql/types/conversation.js
+++ src/graphql/types/conversation.js
@@ -20,7 +20,7 @@ const resolver = {
       }
       await conversation.load(['participants']);
       const participants = conversation.related('participants').toArray();
-      return await Promise.all(participants.map(p => p.serialize()));
+      return await Promise.all(participants.map(participant => participant.serialize()));
     },
 
     messages: async({ id }) => {
@@ -51,7 +51,7 @@ const resolver = {
         })
         .fetchAll();
 
-      return missions.map(mission => mission.serialize());
+      return missions.toArray();
     }
   }
 };
