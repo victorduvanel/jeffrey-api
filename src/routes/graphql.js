d53c5b97e880e6acc181b070f79d4264c16bc956
William Riancho <william@reptilians.io>
2018-06-27 15:51:27 +0200

    fake user added

--- src/routes/graphql.js
+++ src/routes/graphql.js
@@ -2,9 +2,13 @@ import bodyParser                    from 'body-parser';
 import { graphqlExpress }            from 'apollo-server-express';
 import { formatError, GraphQLError } from 'graphql';
 import oauth2                        from '../middlewares/oauth2';
+import User                          from '../models/user';
 import getSchema                     from '../graphql/schema';
 import { InternalError }             from '../graphql/errors';
 import { AppError }                  from '../errors';
+
+// const FAKE_USER = '2b1a5696-11eb-4858-ad1a-6b23c4e478cd';
+const FAKE_USER = null;
 
 export default () => {
   const schema = getSchema();
@@ -12,9 +16,13 @@ export default () => {
   return [
     bodyParser.json(),
     async (req, res, next) => {
-      let authorization = req.get('authorization');
-      if (authorization) {
-        return oauth2(req, res, next);
+      if (FAKE_USER) {
+        req.user = await User.find(FAKE_USER);
+      } else {
+        let authorization = req.get('authorization');
+        if (authorization) {
+          return oauth2(req, res, next);
+        }
       }
       return next();
     },
@@ -27,7 +35,7 @@ export default () => {
         },
         debug: true,
         formatError: (err) => {
-          console.log('####', err.message);
+          console.log('####', err.message, err.locations, err.path);
 
           if (err instanceof GraphQLError) {
             if (!err.originalError || err.originalError instanceof AppError) {
