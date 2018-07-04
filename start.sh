18e0c4ecdd8a0a3a3f192d5b8512b11601095432
Victor Duvanel <victor@duvanel.ch>
2018-07-04 15:38:11 +0200

    update start.sh with appropriate key.json

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/tukula/key.json"
+export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/key.json"
 export GCLOUD_PROJECT="jeffrey-197808"
 
 npm run watch serve
