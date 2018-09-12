3aae9404e3e7f6b915943391eadbb19a59e61757
Emmanuel Gratuze <emmanuelgratuze@MacBook-Pro-de-Emmanuel.local>
2018-09-12 16:24:19 +0200

    Throw error when verification code is wrong

--- src/graphql/errors/index.js
+++ src/graphql/errors/index.js
@@ -1,5 +1,5 @@
 import { GraphQLError } from 'graphql';
-import { AppError}      from '../../errors';
+import { AppError }     from '../../errors';
 
 export const Unauthorized = () => {
   return new AppError('Unauthorized');
