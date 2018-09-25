5cfa6f565a7a80ed42871eae16902b00b4ed9748
Emmanuel Gratuze <emmanuelgratuze@MacBook-Pro-de-Emmanuel.local>
2018-09-25 12:16:03 +0200

    Remove useless file

--- src/models/bank-account.js
+++
@@ -1,6 +0,0 @@
-import bookshelf     from '../services/bookshelf';
-import Base          from './base';
-
-const Business = Base.extend({ }, { });
-
-export default bookshelf.model('Business', Business);
