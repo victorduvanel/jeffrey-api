ce21c44166b83ea67d88dbc70c51885fb71c178d
William Riancho <william@reptilians.io>
2018-10-05 17:46:34 +0200

    check phone number uniqueness

--- src/graphql/mutations/phone-number.js
+++ src/graphql/mutations/phone-number.js
@@ -7,8 +7,8 @@ import User                        from '../../models/user';
 const def = 'phoneNumber(phoneNumber: String!): Boolean';
 const phoneNumber = async (_, { phoneNumber }, { req, user }) => {
 
-  const userWithSameNumber = await User.query('where', 'phone_number', '=', phoneNumber).fetchAll();
-  if (userWithSameNumber.length !== 0) {
+  const userWithSameNumber = await User.query('where', 'phone_number', '=', phoneNumber).count();
+  if (userWithSameNumber !== 0) {
     throw new Error('duplicate_phone_number');
   }
 
