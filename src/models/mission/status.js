fa211e54353ac9538230c22a0ebe4550be5e347b
Emmanuel Gratuze <emmanuel.gratuze@gmail.com>
2018-10-05 17:35:06 +0200

    Phone number duplicates verification and mission status improvements

--- src/models/mission/status.js
+++ src/models/mission/status.js
@@ -9,7 +9,8 @@ export const client = 'client';
 const states = {};
 
 class Status {
-  constructor({ mustBe, triggeredBy } = {}) {
+  constructor({ mustBe, triggeredBy, name } = {}) {
+    this.name = name;
     this.mustBe = !mustBe || Array.isArray(mustBe) ? mustBe : [mustBe];
     this.triggeredBy = triggeredBy || [];
   }
@@ -29,37 +30,44 @@ class Status {
 
 const pending = new Status();
 
-const canceled = new Status({
-  mustBe: pending,
-  triggeredBy: [ provider ]
-});
-
 const accepted = new Status({
+  name: 'accepted',
   mustBe: pending,
   triggeredBy: [ client ]
 });
 
+const canceled = new Status({
+  name: 'canceled',
+  mustBe: [pending, accepted],
+  triggeredBy: [ provider, client ]
+});
+
 const refused = new Status({
+  name: 'refused',
   mustBe: pending,
   triggeredBy: [ client ]
 });
 
 const started = new Status({
+  name: 'started',
   mustBe: accepted,
   triggeredBy: [ provider ]
 });
 
 const aborted = new Status({
+  name: 'aborted',
   mustBe: started,
   triggeredBy: [ client ]
 });
 
 const confirmed = new Status({
+  name: 'confirmed',
   mustBe: started,
   triggeredBy: [ client ]
 });
 
 const terminated = new Status({
+  name: 'terminated',
   mustBe: [started, confirmed],
   triggeredBy: [ client, provider ]
 });
