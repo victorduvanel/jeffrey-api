762f59addb7cc826b71acb892b224a001ad9fb6f
William Riancho <william@reptilians.io>
2018-10-19 20:12:33 +0400

    filter k8s http logs

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -2,6 +2,14 @@ import morgan from 'morgan';
 import config from '../config';
 
 const format = (config.PRODUCTION ) ? 'combined' : 'dev';
-const logger = morgan(format);
+const morganLogger = morgan(format);
+
+const logger = (req, res, next) => {
+  if (req.ip.indexOf('10.0.') === 0 && req.headers['user-agent'].indexOf('kube-probe/') === 0) {
+    next();
+  } else {
+    morganLogger(req, res, next);
+  }
+};
 
 export default logger;
