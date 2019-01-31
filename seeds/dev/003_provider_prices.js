a66069dd6cc0173396322661baf0a86e45b5b642
William Riancho <william@reptilians.io>
2019-01-31 18:06:23 +0100

    wip

--- seeds/dev/003_provider_prices.js
+++ seeds/dev/003_provider_prices.js
@@ -1,9 +1,18 @@
-import uuid from 'uuid';
+import uuid from 'uuid/v4';
 
 exports.seed = async (knex, Promise) => {
   const users = await knex('users').select('id');
 
-  return Promise.map(users, user => {
+  const providerPrices = users.map((user) => ({
+    id: uuid(),
+    userId: user.id,
+    serviceCategoryId: '53cda24f-80e4-4b06-ad55-6827b6a94752',
+    price: 1400,
+    currency: 'EUR',
+    isEnabled: true
+  }));
+
+  return Promise.map(providerPrices, providerPrice => {
     return knex.raw(
       `
         INSERT INTO "provider_prices" (
@@ -14,14 +23,7 @@ exports.seed = async (knex, Promise) => {
         )
         ON CONFLICT DO NOTHING
       `,
-      {
-        id: uuid.v4(),
-        userId: user.id,
-        serviceCategoryId: 'cfceb483-53f9-47fe-bbc4-45ab5375d6aa',
-        price: 1400,
-        currency: 'EUR',
-        isEnabled: true
-      }
+      providerPrice
     );
   });
 };
