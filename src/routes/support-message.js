daf1fb188df4f7855b517377240d23054cd18992
William Riancho <william@reptilians.io>
2019-05-25 12:52:31 +0200

    wip

---
+++ src/routes/support-message.js
@@ -0,0 +1,40 @@
+import bodyParser from 'body-parser';
+import recaptcha from '../services/recaptcha';
+import { sendEmail } from '../services/mailgun';
+import { render } from '../services/handlebars';
+
+export const post = [
+  bodyParser.json(),
+
+  async (req, res) => {
+    const { token, email, firstName, lastName, message } = req.body;
+
+    const captchaRes = await recaptcha(token, req.ip);
+
+    if (captchaRes.score > 0.4) {
+      const content = await render('html/contact-message', {
+        email,
+        firstName,
+        lastName,
+        message
+      });
+
+      await sendEmail({
+        from: 'no-reply@jeffrey.app',
+        to: 'contact@jeffrey.app',
+        subject: '[JEFFREY] New contact',
+        email,
+        message: content
+      });
+
+      res.send({
+        success: true
+      });
+      return;
+    }
+
+    res.send({
+      success: false
+    });
+  }
+];
