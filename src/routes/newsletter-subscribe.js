c327623cdfbf54d3d3137511625eb5c669c6b36f
William Riancho <william@reptilians.io>
2019-06-17 14:14:30 +0200

    recaptacha removed

--- src/routes/newsletter-subscribe.js
+++ src/routes/newsletter-subscribe.js
@@ -1,34 +1,29 @@
 import bodyParser from 'body-parser';
-// import recaptcha from '../services/recaptcha';
 import mailjet from '../services/mailjet';
 
 export const post = [
   bodyParser.json(),
 
   async (req, res) => {
-    const { /* token, */ email } = req.body;
+    const { email } = req.body;
 
     try {
-      // const captchaRes = await recaptcha(token, req.ip);
-
-      // if (captchaRes.score > 0.4) {
-        await mailjet
-          .post('contact')
-          .action('managemanycontacts')
-          .request({
-            ContactsLists:[
-              {
-                ListID: 12526,
-                action: 'addforce'
-              }
-            ],
-            Contacts:[
-              {
-                Email: email
-              }
-            ]
-          });
-      // }
+      await mailjet
+        .post('contact')
+        .action('managemanycontacts')
+        .request({
+          ContactsLists:[
+            {
+              ListID: 12526,
+              action: 'addforce'
+            }
+          ],
+          Contacts:[
+            {
+              Email: email
+            }
+          ]
+        });
 
       res.send({
         success: true
--- src/routes/support-message.js
+++ src/routes/support-message.js
@@ -1,5 +1,4 @@
 import bodyParser from 'body-parser';
-// import recaptcha from '../services/recaptcha';
 import { sendEmail } from '../services/mailgun';
 import { render } from '../services/handlebars';
 
@@ -7,34 +6,25 @@ export const post = [
   bodyParser.json(),
 
   async (req, res) => {
-    const { /* token, */ email, firstName, lastName, message } = req.body;
+    const { email, firstName, lastName, message } = req.body;
 
-    // const captchaRes = await recaptcha(token, req.ip);
+    const content = await render('html/contact-message', {
+      email,
+      firstName,
+      lastName,
+      message
+    });
 
-    // if (captchaRes.score > 0.4) {
-      const content = await render('html/contact-message', {
-        email,
-        firstName,
-        lastName,
-        message
-      });
-
-      await sendEmail({
-        from: 'no-reply@jeffrey.app',
-        to: 'contact@jeffrey.app',
-        subject: '[JEFFREY] New contact',
-        email,
-        message: content
-      });
-
-      res.send({
-        success: true
-      });
-      return;
-    // }
+    await sendEmail({
+      from: 'no-reply@jeffrey.app',
+      to: 'contact@jeffrey.app',
+      subject: '[JEFFREY] New contact',
+      email,
+      message: content
+    });
 
     res.send({
-      success: false
+      success: true
     });
   }
 ];
