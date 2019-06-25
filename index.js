abf736dbc620165917a6226c8bf23113fe33e7d8
William Riancho <william@reptilians.io>
2019-06-25 16:52:39 +0200

    apm added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import apm from 'elastic-apm-node/start';
 import 'loggg';
 
 import commander      from 'commander';
--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/key.json"
 export GCLOUD_PROJECT="jeffrey-197808"
 
+export ELASTIC_APM_SERVICE_NAME="jeffrey-api"
+export ELASTIC_APM_SERVER_URL="http://reptilians.io:8200"
+
 npm run watch serve
